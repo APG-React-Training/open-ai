aa83e92c7e720f461a3f01640a31e70a293d5123
renekrewinkel <rene@krewinkel.nl>
2024-03-07 11:30:49 +0100

    update

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 import './resources/styles/main.css'
+import Button from "./src/components/Button"
 
 const App = () => {
     return(
       <div>
-
+          <Button text={'Text'}/>
+          <Button text={'Image'}/>
       </div>
     )
 }
---
+++ src/components/Button.js
@@ -0,0 +1,11 @@
+const Button = ({ text }) => {
+
+
+  return(
+    <div className={'Button'}>
+      { text }
+    </div>
+  )
+}
+
+export default Button
